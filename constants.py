cd29c415a66524d7a385d83416a932921336336f
toth12 <gabor.toth@yale.edu>
2021-01-13 11:31:30 +0100

    Fortunoff metadata analyzed

--- constants.py
+++ constants.py
@@ -7,3 +7,4 @@ INPUT_FOLDER_FORTUNOFF_FOLIA = os.getcwd()+"/Input/Fortunoff_Folia/"
 #set the pathes of for output folders
 
 WORDCOUNTS = os.getcwd()+"/Output/Wordcounts/"
+BLACKLAB_URL = "http://localhost:8080/blacklab-server-1.7.1/lts/"
