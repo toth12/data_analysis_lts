7dbaa686ba0cb469e20608c2a2f15fecb957482c
toth12 <gabor.toth@yale.edu>
2021-01-14 13:28:11 +0100

    Fortunoff Interviews analyzed

--- constants.py
+++ constants.py
@@ -2,9 +2,9 @@ import sys, os
 
 #set the pathes of the data specific input folders
 INPUT_FOLDER_FORTUNOFF_FOLIA = os.getcwd()+"/Input/Fortunoff_Folia/"
-
+INPUT_FOLDER_FORTUNOFF_METADATA = os.getcwd()+"/Data/Input/Fortunoff_Metadata/"
 
 #set the pathes of for output folders
-
+OUTPUT_FOLDER = os.getcwd()+"/Data/Output/"
 WORDCOUNTS = os.getcwd()+"/Output/Wordcounts/"
 BLACKLAB_URL = "http://localhost:8080/blacklab-server-1.7.1/lts/"
